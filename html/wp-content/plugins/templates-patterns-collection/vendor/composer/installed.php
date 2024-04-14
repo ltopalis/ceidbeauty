971d40ebdda2c41600e47c57c2198e6fedf1f1b5
ltopalis <lazarostop32@gmail.com>
2024-04-14 15:36:16 +0300

    theme and gdpr

---
+++ html/wp-content/plugins/templates-patterns-collection/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/templates-patterns-collection',
+        'pretty_version' => '1.2.8',
+        'version' => '1.2.8.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/templates-patterns-collection' => array(
+            'pretty_version' => '1.2.8',
+            'version' => '1.2.8.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.17',
+            'version' => '3.3.17.0',
+            'reference' => 'b5698f21640d0156bd6777aa5c71e0704748755c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
